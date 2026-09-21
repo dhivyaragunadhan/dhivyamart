9e1c78da28515929adae26c4b2dcbac01f5a2cbc
dhivyaragunadhan <dhivyaragunadhan@gmail.com>
2026-09-21 14:51:35 +0530

    Add files via upload

---
+++ OrderRepository.java
@@ -0,0 +1,9 @@
+package com.dhivya.dhivyamart;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findByBuyerEmail(String buyerEmail);
+}
